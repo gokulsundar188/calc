5d6193c7260916e8ee6400853414d0720489153f
gokulsundar188 <gokulsundar188@gmail.com>
2020-12-21 22:05:20 +0530

    HelloWorld initiated

---
+++ lib/calc.dart
@@ -0,0 +1,12 @@
+library calc;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+  String helloWorld(){
+    return "Helloworld";
+  }
+}
+
+
